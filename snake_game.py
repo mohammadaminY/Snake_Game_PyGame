74f107a371b2ee2051a6c571f29c58f810f5f10f
mohammadamin <mohammadamin>
2021-10-02 09:22:38 +0330

    Update move_on and move and key_handler

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import locals
+import time
 
 
 class Game:
@@ -27,17 +28,19 @@ class Game:
     def move(self, value):
         if value in ('t', 'b', 'r', 'l'):
             self._move = value
-        raise ValueError('The move value must be equal "t" or "b" or "r" or "l"')
+            print(value)
+        else:
+            raise ValueError(f'The move value must be equal "t" or "b" or "r" or "l". value is {value}')
 
-    def move_on(self,move):
-        if move == 't':
-            pass
-        elif move == 'b':
-            pass
-        elif move == 'b':
-            pass
-        elif move == 'b':
-            pass
+    def move_on(self, move, move_num=4):
+        if move == 't' and self.snake.top != 0:
+            self.snake.move_ip(0, -move_num)
+        elif move == 'b' and self.snake.top != (self.screen.get_height() - self.snake.height):
+            self.snake.move_ip(0, move_num)
+        elif move == 'r' and self.snake.left != (self.screen.get_width() - self.snake.width):
+            self.snake.move_ip(move_num, 0)
+        elif move == 'l' and self.snake.left != 0:
+            self.snake.move_ip(-move_num, 0)
 
     def start(self):
         while True:
@@ -49,21 +52,21 @@ class Game:
             self.screen.fill((255, 255, 255))
             self.draw()
             self.key_handler()
+            self.move_on(self.move)
             pygame.display.update()
-
             self.clock.tick(50)
 
     def key_handler(self, move=5):
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_LEFT] and self.snake.left != 0:
-            self.snake.move_ip(-move, 0)
-        elif key[pygame.K_RIGHT] and self.snake.left != (self.screen.get_width() - self.snake.width):
-            self.snake.move_ip(move, 0)
-        elif key[pygame.K_UP] and self.snake.top != 0:
-            self.snake.move_ip(0, -move)
-        elif key[pygame.K_DOWN] and self.snake.top != (self.screen.get_height() - self.snake.height):
-            self.snake.move_ip(0, move)
+        if key[pygame.K_LEFT]:
+            self.move = 'l'
+        elif key[pygame.K_RIGHT]:
+            self.move = 'r'
+        elif key[pygame.K_UP]:
+            self.move = 't'
+        elif key[pygame.K_DOWN]:
+            self.move = 'b'
 
 
 if __name__ == "__main__":
